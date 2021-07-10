4bccecaaa3f531653e5fb2dcc58626a58a5f40e6
mayskiychai <65356923+mayskiychai@users.noreply.github.com>
2021-07-10 15:13:05 +0300

    Update namejs.js

--- task14/namejs.js
+++ task14/namejs.js
@@ -1,5 +1,5 @@
 let namejs = prompt('Какое "официальное" название JavaScript?', '');
-(namejs == 'ECMAScript!' ||
-    'Ecmascript' ||
-    'EcmaScript' ||
-    'ecmascript') ? alert('Верно!') : alert('Не знаете? ECMAScript!');
+(namejs == 'ECMAScript' ||
+           'Ecmascript' ||
+           'EcmaScript' ||
+           'ecmascript') ? alert('Верно!') : alert('Не знаете? ECMAScript!');

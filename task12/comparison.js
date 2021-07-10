cfe31355670f1ce54318ca67483347c2ca404ec0
mayskiychai <65356923+mayskiychai@users.noreply.github.com>
2021-07-10 13:20:18 +0300

    Add files via upload

---
+++ task12/comparison.js
@@ -0,0 +1,7 @@
+console.log(`5 > 4 ? ${5 > 4}`); // true
+console.log(`"ананас" > "яблоко" ? ${"ананас" > "яблоко"}`); // false
+console.log(`"2" > "12" ? ${"2" > "12"}`); // true
+console.log(`undefined == null ? ${undefined == null}`); // true
+console.log(`undefined === null ? ${undefined === null}`); // false
+console.log(`null == "\n0\n" ? ${null == "\n0\n"}`); // fasle
+console.log(`null === +"\n0\n" ? ${null === +"\n0\n"}`); //false

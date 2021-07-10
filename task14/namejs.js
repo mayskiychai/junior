1a3c372dded529a3a51c0ece102b9e5db4be1c83
mayskiychai <65356923+mayskiychai@users.noreply.github.com>
2021-07-10 15:11:35 +0300

    Add files via upload

---
+++ task14/namejs.js
@@ -0,0 +1,5 @@
+let namejs = prompt('Какое "официальное" название JavaScript?', '');
+(namejs == 'ECMAScript!' ||
+    'Ecmascript' ||
+    'EcmaScript' ||
+    'ecmascript') ? alert('Верно!') : alert('Не знаете? ECMAScript!');
